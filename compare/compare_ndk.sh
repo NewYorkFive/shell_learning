d98a59a506805d3dfac86828b549fe8117b979f5
liling <liling@sjdd.com.cn>
2018-01-25 13:53:21 +0800

    update

--- compare/compare_ndk.sh
+++ compare/compare_ndk.sh
@@ -4,9 +4,8 @@ echo "Hello world !"
 
 ndk_13b="/Users/doublel/Documents/Android_NDK/android-ndk-r13b"
 ndk_15c="/Users/doublel/Documents/Android_NDK/android-ndk-r15c"
-ndk_real="/Users/doublel/Documents/Android_NDK/android-ndk-r15c"
 
-if test $ndk_real = $ndk_15c
+if test $ANDROID_NDK = $ndk_15c
 then
 	echo 'equal'
 else
