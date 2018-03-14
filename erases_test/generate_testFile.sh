50da3e9a2e17658b531c1422af29d0980325a264
liling <liling@sjdd.com.cn>
2018-03-14 13:48:21 +0800

    no message

--- erases_test/generate_testFile.sh
+++ erases_test/generate_testFile.sh
@@ -2,7 +2,7 @@
 date="`date +%Y_%m%d_%H%M`"
 # path="`path`"
 # targetFolder="${path}/test_folder"
-currentPath="/Users/Now/shell_learning/erases_test"
+currentPath="/Users/doublel/shell_learning/erases_test"
 targetFolder="$currentPath/test_folder"
 
 testFileName="testFile_${date}.zip"
--- erases_test/scheme_task.sh
+++ erases_test/scheme_task.sh
@@ -1,21 +1,29 @@
 #! /usr/bin/env bash
 date="`date +%Y_%m%d_%H%M`"
 
-currentPath="/Users/Now/shell_learning/erases_test"
+currentPath="/Users/doublel/shell_learning/erases_test"
 targetFolder="$currentPath/test_folder"
-# 20为超过的时间
-# test*为关键字
-fileNames="`find $targetFolder -name \"test*\" -mmin +20`"
 # echo $targetFolder
 
+# testFile*为关键字 
+# attention!! 路径上不要包含这个关键字，如果用test做关键字，会导致test_folder中所有路径文件夹都会被显示
+# attention!! 或者你可以采取用cd这个文件夹，然后进行删除
+fileNames="`find $targetFolder -name \"testFile*\" | xargs ls -t`"
 # echo $fileNames
 
+leftThreshold=10
+count=0
 for fileName in $fileNames
 do
     # echo $fileName
-    rm -rf $fileName
-    theCommand="rm -rf $fileName"
-    echo $theCommand
-    echo $theCommand >> "$targetFolder/log.txt"
+    count=`expr $count + 1`
+    # echo "count = $count"
+    if [ $count -gt $leftThreshold ]; then
+    	
+	    # rm -rf $fileName
+    	theCommand="rm -rf $fileName"
+    	echo $theCommand    	
+	    echo $theCommand >> "$targetFolder/log.txt"
+    fi
 done
 
